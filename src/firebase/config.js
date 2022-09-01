86ef6678ce6373295f67d17aa8a5f51a86665048
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-09-01 16:55:35 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firease/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcmVEPY6pFVWy-74gTvjfjcypPbVsjGbM",
+  authDomain: "mymoney-9c685.firebaseapp.com",
+  projectId: "mymoney-9c685",
+  storageBucket: "mymoney-9c685.appspot.com",
+  messagingSenderId: "1030504576271",
+  appId: "1:1030504576271:web:94b199d0d2119a1696fa7a"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore }
