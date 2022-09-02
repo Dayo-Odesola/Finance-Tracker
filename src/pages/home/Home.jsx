fb51094a70da8199e94919c335f05d685ddace39
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-09-02 21:05:30 +0100

    added transaction form

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import styles from "./Home.module.css"
 import { useTheme } from "../../hooks/useTheme"
 
+
+// components 
+import TransactionForm from "./TransactionForm"
+
 export default function Home() {
 
   const { mode } = useTheme()
   return (
-    <div className={`${styles[`home`]} ${styles[`${mode}`]}`}>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   )
 }
